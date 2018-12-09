98e07d4d647048fd2ab947284ba51bfc19bf9683
Kelly Page <kellypage.00@gmail.com>
2018-12-08 22:23:49 -0500

    add more dependencies
    Co-authored-by: Michael Page <42724120+MPage68@users.noreply.github.com>

--- src/main/java/com/pagecomp/brewapp/BrewRecipeApplication.java
+++ src/main/java/com/pagecomp/brewapp/BrewRecipeApplication.java
@@ -6,7 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrewRecipeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BrewRecipeApplication.class, args);
-	}
+	public static void main(String[] args) {	SpringApplication.run(BrewRecipeApplication.class, args);	}
 }
